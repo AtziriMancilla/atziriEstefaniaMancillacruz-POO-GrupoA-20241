55b986b0ccc69f1d078b34624cd9b891b0c69524
Atziri Mancilla <atzirimc22@gmail.com>
2024-02-27 19:50:09 -0600

    main agregado

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Producto producto1=new Producto("Papa",15.5);
+        Producto producto2=new Producto("Papel",32.8,12);
 
+        System.out.println("El stock de: "+producto1.getNombre()+" es "+producto1.getStock());
+        producto1.setStock(12);
+        System.out.println("El stock de: "+producto1.getNombre()+" es "+producto1.getStock());
+        System.out.println("El precio de: "+producto2.getNombre()+" es "+producto2.getPrecio());
+        producto2.aumentarStock(0);
     }
 }
